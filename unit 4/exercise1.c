1cef3fd4852e7ef70560b9ad62c982a0f362c97c
RA1911031010095shubham <91885458+RA1911031010095shubham@users.noreply.github.com>
2021-10-04 18:10:30 +0530

    Add files via upload

---
+++ unit 4/exercise1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int year ;
+
+    printf("please enter the year\n");
+    scanf("%d" , &year);
+
+    if ( year %4 == 0)
+    {
+        printf("the year is a leap year\n");
+    }
+    else
+    printf("the year is not a leap year\n");
+
+    return 0;
+    
+}
